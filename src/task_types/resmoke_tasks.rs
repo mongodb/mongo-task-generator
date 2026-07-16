8d806e2cb0aafd139eb0a0bdb9fb4e0012c73680
Eric Lavigne <eric.lavigne@mongodb.com>
2026-07-16 12:05:09 -0600

    Address review: remove negative gen_task_config_location assertions

--- src/task_types/resmoke_tasks.rs
+++ src/task_types/resmoke_tasks.rs
@@ -956,7 +956,6 @@ mod tests {
         let test_vars = params.build_run_test_vars("my_suite_0", &sub_suite, "", None);
 
         assert_eq!(test_vars.len(), 3);
-        assert!(!test_vars.contains_key("gen_task_config_location"));
         assert!(!test_vars.contains_key("resmoke_jobs_max"));
         assert_eq!(
             test_vars.get("suite").unwrap(),
@@ -984,7 +983,6 @@ mod tests {
         let test_vars = params.build_run_test_vars("my_suite_0", &sub_suite, "", None);
 
         assert_eq!(test_vars.len(), 4);
-        assert!(!test_vars.contains_key("gen_task_config_location"));
         assert_eq!(
             test_vars.get("resmoke_jobs_max").unwrap(),
             &ParamValue::from(5)
@@ -1017,7 +1015,6 @@ mod tests {
             params.build_run_test_vars("my_suite_0", &sub_suite, "tag_0,tag_1,tag_2", None);
 
         assert_eq!(test_vars.len(), 4);
-        assert!(!test_vars.contains_key("gen_task_config_location"));
         assert_eq!(
             test_vars.get("multiversion_exclude_tags_version").unwrap(),
             &ParamValue::from("last_lts")
@@ -1047,7 +1044,6 @@ mod tests {
         let test_vars = params.build_run_test_vars("my_suite_0", &sub_suite, "", None);
 
         assert_eq!(test_vars.len(), 4);
-        assert!(!test_vars.contains_key("gen_task_config_location"));
         assert_eq!(
             test_vars.get("multiversion_exclude_tags_version").unwrap(),
             &ParamValue::from("last_lts")
@@ -1079,7 +1075,6 @@ mod tests {
         let test_vars = params.build_run_test_vars("my_suite_0", &sub_suite, "", None);
 
         assert_eq!(test_vars.len(), 4);
-        assert!(!test_vars.contains_key("gen_task_config_location"));
         assert_eq!(
             test_vars.get("multiversion_exclude_tags_version").unwrap(),
             &ParamValue::from("last_lts")
